7d041cc8a4b90c7b3427638bb5c3a0aad58d1ac9
araki-shogo <shogo.araki.shogo.araki@gmail.com>
2023-09-14 19:31:29 +0900

    initial commit

---
+++ page-cooler.php
@@ -0,0 +1,14 @@
+<?php
+/*
+*Template Name: cooler
+*/
+$home_url = esc_url(home_url());
+$theme_url = get_template_directory_uri();
+get_header();
+?>
+
+<div class="p-cooler">
+
+</div>
+
+<?php get_footer(); ?>
---
+++ page-icemaker.php
@@ -0,0 +1,14 @@
+<?php
+/*
+*Template Name: icemaker
+*/
+$home_url = esc_url(home_url());
+$theme_url = get_template_directory_uri();
+get_header();
+?>
+
+<div class="p-ice">
+
+</div>
+
+<?php get_footer(); ?>
