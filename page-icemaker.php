a7993e0b5127db3483557f54807aa8a971fc5c65
araki-shogo <shogo.araki.shogo.araki@gmail.com>
2023-09-15 18:57:40 +0900

    pc icemaker page

--- page-icemaker.php
+++ page-icemaker.php
@@ -8,7 +8,233 @@ get_header();
 ?>
 
 <div class="p-ice">
-
+    <div class="p-ice_wrap">
+        <h1>店舗規定による機種選定の目安</h1>
+        <div class="p-ice_wrap_table">
+            <table class="p-ice_wrap_table_wrap">
+                <tbody class="p-ice_wrap_table_wrap_tbody">
+                    <tr class="p-ice_wrap_table_wrap_tbody_tr">
+                        <td rowspan="2">業務</td>
+                        <td>一般飲食店<br>レストラン(席数)</td>
+                        <td>10席</td>
+                        <td>20席</td>
+                        <td>30席</td>
+                        <td>40席</td>
+                        <td>60席</td>
+                        <td>100席～</td>
+                    </tr>
+                    <tr class="p-ice_wrap_table_wrap_tbody_tr">
+                        <td>
+                            居酒屋・スナック・<br>
+                            ファーストフード<br>
+                            (1時間当の客数)
+                        </td>
+                        <td>10人</td>
+                        <td>20人</td>
+                        <td>30人</td>
+                        <td>40人</td>
+                        <td>60人</td>
+                        <td>100人</td>
+                    </tr>
+                    <tr class="p-ice_wrap_table_wrap_tbody_tr">
+                        <td>能力</td>
+                        <td>製氷能力 / 日</td>
+                        <td>25㎏～45㎏</td>
+                        <td>45㎏～60㎏</td>
+                        <td>65㎏～75㎏</td>
+                        <td>85㎏～115㎏</td>
+                        <td>130㎏～</td>
+                        <td>200㎏～</td>
+                    </tr>
+                    <tr class="p-ice_wrap_table_wrap_tbody_tr">
+                        <td rowspan="4">
+                            <div>
+                                <img src="<?php echo $theme_url ?>/images/icemaker/cubeice.png">
+                                <span>キューブアイス</span>
+                            </div>
+                        </td>
+                        <td>
+                            <img src="<?php echo $theme_url ?>/images/icemaker/HOSHIZAKI.png">
+                        </td>
+                        <td>
+                            <a href="">IM-25M-2</a>
+                            <a href="">IM-35M-2</a>
+                        </td>
+                        <td>
+                            <a href="">IM-45M-2</a>
+                            <a href="">IM-55M-2</a>
+                        </td>
+                        <td>
+                            <a href="">IM-65M-2</a>
+                            <a href="">IM-75TM-1</a>
+                        </td>
+                        <td>
+                            <a href="">IM-95TM-1</a>
+                            <a href="">IM-115M-1</a>
+                        </td>
+                        <td>
+                            <a href="">IM-180DM-1-STN</a>
+                        </td>
+                        <td>
+                            <a href="">IM-230M-1</a>
+                        </td>
+                    </tr>
+                    <tr class="p-ice_wrap_table_wrap_tbody_tr">
+                        <td>
+                            <img src="<?php echo $theme_url ?>/images/icemaker/panasonic.png">
+                        </td>
+                        <td>
+                            <a href="">SIM-AS2500</a>
+                            <a href="">SIM-AS3500</a>
+                        </td>
+                        <td>
+                            <a href="">SIM-AS4500</a>
+                            <a href="">SIM-S5500B</a>
+                        </td>
+                        <td>
+                            <a href="">SIM-S6500B</a>
+                            <a href="">SIM-S7500UB</a>
+                        </td>
+                        <td>
+                            <a href="">SIM-AS4500</a>
+                            <a href="">SIM-S5500B</a>
+                        </td>
+                        <td>
+                            <a href="">SIM-S140XNB-LB2</a>
+                        </td>
+                        <td>
+                            <a href="">SIM-S241VNB</a>
+                            <a href="">SIM-S241VNSB</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src="<?php echo $theme_url ?>/images/icemaker/GALILEI.png">
+                        </td>
+                        <td>
+                            <a href="">FIC-A25KT5</a>
+                            <a href="">FIC-A35KT5</a>
+                        </td>
+                        <td>
+                            <a href="">FIC-A45KT3</a>
+                        </td>
+                        <td>
+                            <a href="">FIC-A65KV3</a>
+                            <a href="">FIC-A75KT3</a>
+                        </td>
+                        <td>
+                            <a href="">FIC-A95KV3</a>
+                            <a href="">FIC-A95KT3</a>
+                        </td>
+                        <td>―</td>
+                        <td>
+                            <a href="">FIC-A240KL2T</a>
+                            <a href="">FIC-A240KV2S</a>
+                        </td>
+                    </tr>
+                    <tr class="p-ice_wrap_table_wrap_tbody_tr">
+                        <td>
+                            <img src="<?php echo $theme_url ?>/images/icemaker/DAIWA.png">
+                        </td>
+                        <td>
+                            <a href="">DRI-25LMF</a>
+                            <a href="">DRI-35LMF</a>
+                        </td>
+                        <td>
+                            <a href="">DRI-45LMF</a>
+                            <a href="">DRI-55LMTF</a>
+                        </td>
+                        <td>
+                            <a href="">DRI-65LMF</a>
+                            <a href="">FIC-A75KT3</a>
+                        </td>
+                        <td>
+                            <a href="">DRI-95LMTF</a>
+                            <a href="">DRI-110LMV1</a>
+                        </td>
+                        <td>
+                            <a href="">DRI-110LM2-B</a>
+                            <a href="">DRI-150LM2-B</a>
+                        </td>
+                        <td>
+                            <a href="">DRI-210LM1-BS</a>
+                            <a href="">DRI-230LM-SAB</a>
+                        </td>
+                    </tr>
+                    <tr class="p-ice_wrap_table_wrap_tbody_tr">
+                        <td rowspan="2">
+                            <div>
+                                <img src="<?php echo $theme_url ?>/images/icemaker/tipice.png">
+                                <span>チップアイス</span>
+                            </div>
+                        </td>
+                        <td>
+                            <img src="<?php echo $theme_url ?>/images/icemaker/panasonic.png">
+                        </td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>
+                            <a href="">SIM-C120B</a>
+                        </td>
+                        <td>
+                            <a href="">SIM-C120B</a>
+                        </td>
+                        <td>
+                            <a href="">SIM-C120LB</a>
+                        </td>
+                        <td>
+                            <a href="">SIM-AS240YN-FXB4</a>
+                        </td>
+                    </tr>
+                    <tr class="p-ice_wrap_table_wrap_tbody_tr">
+                        <td>
+                            <img src="<?php echo $theme_url ?>/images/icemaker/HOSHIZAKI.png">
+                        </td>
+                        <td>-</td>
+                        <td>
+                            <a href="">CM-60A</a>
+                        </td>
+                        <td>
+                            <a href="">CM-60A</a>
+                            <a href="">CM-100K</a>
+                        </td>
+                        <td>
+                            <a href="">CM-100K</a>
+                            <a href="">CM-100K-50</a>
+                        </td>
+                        <td>
+                            <a href="">CM-100K-50</a>
+                            <a href="">CM-200K</a>
+                        </td>
+                        <td>
+                            <a href="">CM-200K</a>
+                        </td>
+                    </tr>
+                    <tr class="p-ice_wrap_table_wrap_tbody_tr">
+                        <td>
+                            <div>
+                                <img src="<?php echo $theme_url ?>/images/icemaker/flakeice.png">
+                                <span>フレークアイス</span>
+                            </div>
+                        </td>
+                        <td>
+                            <img src="<?php echo $theme_url ?>/images/icemaker/panasonic.png">
+                        </td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>
+                            <a href="">SIM-F140B</a>
+                        </td>
+                        <td>
+                            <a href="">SIM-F140LB</a>
+                        </td>
+                        <td>-</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 
 <?php get_footer(); ?>
